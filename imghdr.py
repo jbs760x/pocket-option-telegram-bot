9749b021eb2cf40d2d10f5db96d640bc383178a0
jbs760x <jbs760x@gmail.com>
2025-10-02 06:15:05 -0700

    Delete imghdr.py

--- imghdr.py
+++
@@ -1,4 +0,0 @@
-# Minimal stub so older libraries that import 'imghdr' don't crash on Python 3.13+
-# Our bot doesn't upload image files, so simple None detection is fine.
-def what(file, h=None):
-    return None
